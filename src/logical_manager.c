054cf56b5545ab3282f651f4a72755c2f0073324
DarquesTheo <theo.darques@epitech.eu>
2024-04-23 15:34:28 +0200

    Added str_to_arr for multiple char separator

--- src/logical_manager.c
+++ src/logical_manager.c
@@ -10,12 +10,10 @@
 int logical_manager(char *input, int *child_return, pipe_t *pip)
 {
     if (input[0] == '&' && input[1] == '&') {
-        printf("and detected %d\n", *child_return);
         if (*child_return == 0) {
             pip->can_execute = 1;
             return 0;
         }
-        printf("next command will be skipped\n");
         pip->can_execute = 0;
         return 0;
     }
--- src/str_to_arr_word.c
+++ src/str_to_arr_word.c
@@ -7,18 +7,31 @@
 
 #include "my.h"
 
+static int is_separator(char *str, char *word, int i)
+{
+    for (int j = 0; str[i + j] != 0 && word[j] != 0; j++) {
+        if (str[i + j] != word[j])
+            return 0;
+    }
+    return 1;
+}
+
 static int count_words(char *str, char *word)
 {
-    char *tmp = str;
     int words = 0;
-    int run = 1;
+    int last = 0;
 
-    while (run) {
-        tmp = strstr(tmp + 1, word);
-        if (tmp == NULL)
-            run = 0;
-        if (tmp != NULL)
+    for (int i = 0; str[i] != 0; i++) {
+        if (is_separator(str, word, i)) {
+            if (i > 0)
+                words++;
+            last = i;
             words++;
+            i++;
+        }
+    }
+    if (last < my_strlen(str) - my_strlen(word) - 1) {
+        words++;
     }
     return words + 1;
 }
@@ -26,19 +39,19 @@ static int count_words(char *str, char *word)
 char **str_to_arr_word(char *str, char *word)
 {
     char **arr = malloc(sizeof(char *) * (count_words(str, word) + 1));
-    char *tmp = str;
     int start = 0;
-    int end = 0;
+    int curr = 0;
 
-    while (tmp != NULL) {
-        tmp = strstr(tmp, word);
-        if (tmp == NULL)
-            continue;
-        end = tmp - str - 1;
-        char *jsp = copy_string_between(str, start, end);
-        printf("%s %d %d %s\n", tmp, start, end, jsp);
-        start = tmp - str;
-        tmp++;
+    for (int i = 0; str[i] != 0; i++) {
+        if (is_separator(str, word, i)) {
+            arr[curr] = copy_string_between(str, start, i - 1);
+            arr[curr + 1] = copy_string_between(str, i, i + my_strlen(word) - 1);
+            start = i + 2;
+            curr += 2;
+        }
     }
-
+    if (my_strlen(word) != my_strlen(str)) {
+        arr[curr] = copy_string_between(str, start, my_strlen(str) - 1);
+    }
+    return arr;
 }
